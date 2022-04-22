6e52f0a8938b33d34efc08453c3dc81172e61a9e
Rafael Araujo <rflaraujodev@gmail.com>
2022-04-22 11:53:01 -0300

    info router + response time

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const { resolve } = require("path");
+const responseTime = require('response-time');
 const fs = require("fs");
 class App {
     constructor() {
@@ -13,6 +14,8 @@ class App {
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.json());
         this.app.use(express.static(resolve(__dirname, "public")));
+        this.app.use(responseTime({ digits: 2 }));
+        this.app.use(responseTime((req, res, time) => { console.log(`${req.method} ${req.url} ${time.toString().substring(0, 4)}ms`); }));
     }
     setRoutes() {
         const routeDirectory = resolve(__dirname, 'routes')
